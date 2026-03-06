711dca201b517449ee9ddabbaa8d076a95c4d526
kps33 <krishnasadariya2476@gmail.com>
2026-03-07 02:08:58 +0530

    "formating"

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
         seed: "tsx prisma/seed.ts",
     },
     datasource: {
-        url: env("DATABASE_URL"),
+        url: process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
     },
 });
